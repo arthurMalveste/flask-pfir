5dae709eb79c1f2df7c9c5a1db1e175373f6b4ff
Arthur Malveste <arthurmielemalveste@gmail.com>
2023-11-06 20:10:27 -0300

    add biblioteca e conexão

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from flask import Flask, jsonify, render_template
 import os
+from flask import Flask, jsonify, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask import request
+import datetime
+from datetime import date, datetime, timezone, timedelta
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://Arthur_Malveste:Amm.22.01.78@localhost/portaria'
+app.config['TIMEZONE'] = 'America/Sao_Paulo'  # Substitua pelo seu fuso horário
+db = SQLAlchemy(app)
+
 
 app = Flask(__name__)
 
