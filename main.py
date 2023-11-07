cf26b7ff4c8d9ad4c6ea05d20b770da976a49d5a
Arthur Malveste <arthurmielemalveste@gmail.com>
2023-11-06 21:06:32 -0300

    sei la tmb

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
-from flask import Flask, jsonify, render_template
-import os
-from flask import Flask, jsonify, render_template, request, redirect
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask import request
-import datetime
-from datetime import date, datetime, timezone, timedelta
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://portaria_xgw7_user:67o65aAyM9aahzZrcEFYNpfGj9WBqORP@dpg-cl4nn3h828mc73cvtbrg-a.oregon-postgres.render.com/portaria_xgw7'
 app.config['TIMEZONE'] = 'America/Sao_Paulo'  # Substitua pelo seu fuso horário
 db = SQLAlchemy(app)
 
-
-
-
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-
 @app.route('/tabela')
-def index():
+def tabela():
     return render_template('tabela.html')
 
-
-if __name__ == '__main__':
+if __name__ == '__main':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
